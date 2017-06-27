89f43e51a20f93cc9cd53290ed9e174d02b9e3a0
xiaojieluo <xiaojieluoff@gmail.com>
2017-06-27 11:12:46 +0800

    clean

--- admin/copyright.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('__TYPECHO_ADMIN__')) exit; ?>
-<div class="typecho-foot" role="contentinfo">
-    <div class="copyright">
-        <p><?php _e('由 <a href="http://localhost">%s</a> 强力驱动, 版本 %s (%s)', $options->software, $prefixVersion, $suffixVersion); ?></p>
-    </div>
-</div>
